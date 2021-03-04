a37913f73a3b522e6f905ca97d8bedf8a68bfd7e
Tobby Lie <tobbylie@gmail.com>
2021-03-04 11:52:20 -0700

    updated /movies post route

--- server.js
+++ server.js
@@ -109,10 +109,10 @@ router.route('/movies')
         }
     })
     .put(authJwtController.isAuthenticated, function (req, res) {
-        if (!req.body.find_title || !req.body.update_data) {
+        if (!req.body.find_title || !req.body.update_title) {
             return res.json({ success: false, message: "Please provide a movie to be updated as well as the new data to update that movie." });
         } else {
-            Movie.findOneAndUpdate( {title: req.body.find_title}, req.body.update_data, function (err, doc) {
+            Movie.findOneAndUpdate( {title: req.body.find_title}, req.body.update_title, function (err, doc) {
                 if (err) {
                     return res.status(403).json({success: false, message: "Unable to update movie passed in."});
                 } else {
