2aff2abed25cddd5b66b6b9b8b660afeff8933c7
Tobby Lie <tobbylie@gmail.com>
2021-03-04 13:16:52 -0700

    updated /movies post route

--- server.js
+++ server.js
@@ -143,11 +143,11 @@ router.route('/movies')
         if (!req.body.find_title) {
             return res.json({ success: false, message: "Please provide a title to be retrieved." });
         } else {
-            Movie.findOne( {title: req.body.find_title}).select("title year_released genre actors").exec(function (err, movie) {
+            Movie.find( req.body.find_title).select("title year_released genre actors").exec(function (err, movie) {
                 if (err) {
                     return res.status(403).json({success: false, message: "Unable to retrieve title passed in."});
                 }
-                if (movie && movie.length > 0) {
+                if (movie) {
                     return res.status(200).json({success: true, message: "Successfully retrieved movie.", movie: movie});
                 } else {
                     return res.status(404).json({success: false, message: "Unable to retrieve a match for title passed in."});
