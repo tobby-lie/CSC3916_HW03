ff05702652c231fbbf0787ae3030c890d0dff928
Tobby Lie <tobbylie@gmail.com>
2021-03-05 19:55:07 -0700

    updated /movies post route

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ router.route('/movies')
                 if (err) {
                     return res.status(403).json({success: false, message: "Unable to retrieve title passed in."});
                 }
-                if (movie) {
+                if (movie && movie.length > 0) {
                     return res.status(200).json({success: true, message: "Successfully retrieved movie.", movie: movie});
                 } else {
                     return res.status(404).json({success: false, message: "Unable to retrieve a match for title passed in."});
